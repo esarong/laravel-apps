b15d8e9f588c8bddd9cc5e5fd4fea60fac3a617a
esarong <esarong@student.apc.edu.ph>
2023-11-27 12:48:35 +0800

    First Commit to the internet laravel-app

---
+++ app/Http/Resources/PetitionCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PetitionCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * //@param \Illuminate\Http\Request  $request
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection,
+       ];
+       
+       
+    }
+}
---
+++ database/seeders/PetitionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Petition;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PetitionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Petition::factory()->times(count: 50)->create();
+        
+        //
+    }
+}
